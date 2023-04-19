ba75dde8961fc2cf28c91116b988a414429ae746
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-20 00:21:37 +0400

    fix: fixed height to table success animation language arrow

--- resources/views/dashboard/bycountry.blade.php
+++ resources/views/dashboard/bycountry.blade.php
@@ -3,71 +3,67 @@
         <form method="GET" action="#" class="pb-10">
             <x-form.search name="search" />
         </form>
-        <table class="w-full absolute border-collapse md:relative lg:relative left-0 ">
-            <thead>
-                <tr class="bg-gray-50 ">
-                    <th class="py-2 px-4 border-b border-gray-100 text-left font-semibold text-sm whitespace-nowrap">
-                        <a
-                            href="{{route('dashboard.bycountry', ['search' => request('search'),'country' => request('country') === 'desc' ? 'asc' : 'desc'])}}"
-                            >
-                            {{__('dashboard.location')}}
-                            <span class="absolute ml-2 mt-1">
-                                <x-assets.arrow-up />
-                                <x-assets.arrow-down />
-                            </span>
-                        </a>
-                    </th>
-                    <th class="py-2 px-4 border-b border-gray-100 text-left font-semibold text-sm whitespace-nowrap">
-                        <a 
-                        href="{{route('dashboard.bycountry', ['search' => request('search'),'confirmed' => request('confirmed') === 'desc' ? 'asc' : 'desc'])}}"
-                        > {{__('dashboard.new_cases')}}
-                            <span class="absolute ml-2 mt-1">
-                                <x-assets.arrow-up />
-                                <x-assets.arrow-down />
-                            </span>
-                        </a>
-                    </th>
-                    <th class="py-2 px-4 border-b border-gray-100 text-left font-semibold text-sm">
-                        <a 
-                        href="{{route('dashboard.bycountry', ['search' => request('search'),'deaths' => request('deaths') === 'desc' ? 'asc' : 'desc'])}}"
-                        > {{__('dashboard.death')}}
-                            <span class="absolute ml-2 mt-1">
-                                <x-assets.arrow-up />
-                                <x-assets.arrow-down />
-                            </span>
-                        </a>
-                    </th>
-                    <th class="py-2 px-4 border-b border-gray-100 text-left font-semibold text-sm">
-                        <a 
-                        href="{{route('dashboard.bycountry', ['search' => request('search'),'recovered' => request('recovered') === 'desc' ? 'asc' : 'desc'])}}"
-                        >
-                            {{__('dashboard.recovered')}}
-                            <span class="absolute ml-2 mt-1">
-                                <x-assets.arrow-up />
-                                <x-assets.arrow-down />
-                            </span>
-                        </a>
-                    </th>
-                </tr>
-            </thead>
-            @foreach ($countries as $country)
-            <tbody>
-                <tr class="hover:bg-gray-50 flex-column">
-                    <td class="py-2 px-4 border-t border-b border-gray-100 w-1/4">
+        <div>
+            <div class="bg-gray-50 flex">
+                <div class="py-2 px-4 border-b border-gray-100 text-left font-semibold text-sm w-1/4">
+                    <a
+                        href="{{ route('dashboard.bycountry', ['search' => request('search'),'country' => request('country') === 'desc' ? 'asc' : 'desc']) }}">
+                        {{ __('dashboard.location') }}
+                        <span class="absolute ml-2 mt-1">
+                            <x-assets.arrow-up />
+                            <x-assets.arrow-down />
+                        </span>
+                    </a>
+                </div>
+                <div class="py-2 px-4 border-b border-gray-100 text-left font-semibold text-sm w-1/4">
+                    <a
+                        href="{{ route('dashboard.bycountry', ['search' => request('search'),'confirmed' => request('confirmed') === 'desc' ? 'asc' : 'desc']) }}">
+                        {{ __('dashboard.new_cases') }}
+                        <span class="absolute ml-2 mt-1">
+                            <x-assets.arrow-up />
+                            <x-assets.arrow-down />
+                        </span>
+                    </a>
+                </div>
+                <div class="py-2 px-4 border-b border-gray-100 text-left font-semibold text-sm w-1/4">
+                    <a
+                        href="{{ route('dashboard.bycountry', ['search' => request('search'),'deaths' => request('deaths') === 'desc' ? 'asc' : 'desc']) }}">
+                        {{ __('dashboard.death') }}
+                        <span class="absolute ml-2 mt-1">
+                            <x-assets.arrow-up />
+                            <x-assets.arrow-down />
+                        </span>
+                    </a>
+                </div>
+                <div class="py-2 px-4 border-b border-gray-100 text-left font-semibold text-sm w-1/4">
+                    <a
+                        href="{{ route('dashboard.bycountry', ['search' => request('search'),'recovered' => request('recovered') === 'desc' ? 'asc' : 'desc']) }}">
+                        {{ __('dashboard.recovered') }}
+                        <span class="absolute ml-2 mt-1">
+                            <x-assets.arrow-up />
+                            <x-assets.arrow-down />
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="w-full h-[38rem] overflow-y-scroll scrollbar-thumb-gray-500 scrollbar-track-gray-300 scrollbar-thin">
+                @foreach ($countries as $country)
+                <div class="flex bg-white hover:bg-gray-50">
+                    <div class="py-2 px-4 border-t border-b border-gray-100 w-1/4">
                         {{ json_decode($country->country, true)[Session::get('locale')] }}
-                    </td>
-                    <td class="py-2 px-4 border-t border-b border-gray-100 w-1/4">{{number_format($country->confirmed)}}
-                    </td>
-                    <td class="py-2 px-4 border-t border-b border-gray-100 w-1/4">{{number_format($country->deaths)}}
-                    </td>
-                    <td class="py-2 px-4 border-t border-b border-gray-100 w-1/4">{{number_format($country->recovered)}}
-                    </td>
-                </tr>
-            </tbody>
-            @endforeach
-        </table>
+                    </div>
+                    <div class="py-2 px-4 border-t border-b border-gray-100 w-1/4">{{ number_format($country->confirmed)
+                        }}
+                    </div>
+                    <div class="py-2 px-4 border-t border-b border-gray-100 w-1/4">{{ number_format($country->deaths) }}
+                    </div>
+                    <div class="py-2 px-4 border-t border-b border-gray-100 w-1/4">{{ number_format($country->recovered)
+                        }}
+                    </div>
+                </div>
+                @endforeach
+            </div>
 
-
-
+        </div>
     </div>
 </x-dashboard-layout>
