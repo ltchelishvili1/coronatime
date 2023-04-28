524a7993708b174c6037408f4bce9db9943b41d0
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-28 16:18:47 +0400

    fix: auth sizes

--- resources/views/components/auth-layout.blade.php
+++ resources/views/components/auth-layout.blade.php
@@ -2,12 +2,12 @@
     <div class="flex flex-col md:flex-row ml-4">
         <div class="flex-1 mx-2 md:mx-28 mt-11">
            <a href="{{route('login.index')}}"> <x-assets.coronatime-logo /></a>
-            <div class="mt-14">
+            <div class="mt-14 md:min-w-min	">
                 {{$slot}}
             </div>
         </div>
-        <div class="md:block hidden">
-           <img src="{{asset('images/coronatime-background.jpg')}}" />
+        <div class="md:block hidden w-1/2">
+           <img class="h-screen  ml-auto" src="{{asset('images/coronatime-background.jpg')}}" />
         </div>
     </div>
 </x-layout>
